ad18d593e75950fb71a56c0e421fb53deb50f67c
sahanilxm <laxmisahani240@gmail.com>
2024-02-14 04:58:24 +0530

    jj

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -4,7 +4,7 @@ import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['../../styles.scss']
+  styleUrls: ['../../styles.scss',]
 })
 export class HomeComponent{
 
--- src/app/home/user/profile/profile.component.ts
+++ src/app/home/user/profile/profile.component.ts
@@ -3,7 +3,7 @@ import { Component, Input} from '@angular/core';
 @Component({
   selector: 'app-profile',
   templateUrl: './profile.component.html',
-  styleUrls: ['../../../../styles.css']
+  styleUrls: ['../../../../styles.css',]
 })
 export class ProfileComponent{
 
